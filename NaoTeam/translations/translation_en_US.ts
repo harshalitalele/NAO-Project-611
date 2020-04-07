cd50dcc9806a51e4761a177eb72792ef89d4f091
buffscirobotics <robotics@bascs.org>
2020-04-07 11:10:57 -0400

    initial commit

---
+++ NaoTeam/translations/translation_en_US.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, I did not understand you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, I did not understand you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am already standing</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am already standing</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I will stand up now, I got this!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I will stand up now, I got this!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am already sitting.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am already sitting.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I will sit down now.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I will sit down now.</translation>
+        </message>
+    </context>
+</TS>
