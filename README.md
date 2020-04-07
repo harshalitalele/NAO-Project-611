aad31eb59651de23213adf23e39fee1046645b08
buffsci-borgs <robotics@bascs.org>
2020-04-07 13:26:39 -0400

    will fix reading parameter input

---
+++ README.md
@@ -0,0 +1,1 @@
+http://doc.aldebaran.com/2-8/software/choregraphe/objects/python_script.html
--- ReadBook/translations/translation_en_US.ts
+++ ReadBook/translations/translation_en_US.ts
@@ -4,10 +4,9 @@
     <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
-            <translation>Hello</translation>
+            <translation type="vanished">Hello</translation>
         </message>
     </context>
 </TS>
