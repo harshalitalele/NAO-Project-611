188dd3f9a6f18ef4103f9531b95b621ff73fcfb4
harshalitalele <harshalitalele@gmail.com>
2020-04-15 15:35:32 -0400

    Game beginning added

--- RockPaperScissors/translations/translation_en_US.ts
+++ RockPaperScissors/translations/translation_en_US.ts
@@ -9,4 +9,51 @@
             <translation type="vanished">Hello</translation>
         </message>
     </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline (1)/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Rock Paper Scissor</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Rock Paper Scissor</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Paper</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Paper</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline (2)/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Rock Paper Scissor</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Rock Paper Scissor</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Scissor</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Scissor</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Rock Paper Scissor</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Rock Paper Scissor</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Rock</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Rock</translation>
+        </message>
+    </context>
 </TS>
